2ad2dd7f8039ab146f0b2975bd991002bba243f6
hasanfur <aa@aa.pl>
2025-05-29 19:11:29 +0200

    init

---
+++ main.py
@@ -0,0 +1,101 @@
+message = "Hello"
+print(message)
+
+message = message + "World"
+print(message)
+
+# Integer
+counter = 2
+print(counter)
+
+# Floating-point number
+weight_sum = 10.5
+print(weight_sum)
+
+# Boolean
+always_true = True
+print(always_true)
+never_true = False
+print(never_true)
+
+# String
+message2 = "Message"
+print(message2)
+message3 = """
+Hello 
+Wolrd
+123
+xyz
+"""
+print(message3)
+
+# None
+nothing_here = None
+print(nothing_here)
+
+# Operations on variables
+a = 1.5
+b = 0.5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+a = 2
+b = 3
+
+print(a ** b)  # 2 * 2 * 2
+print(b % a)  # Rest from divide
+
+# Comparing
+print(a == b)
+print(a != b)
+print(a < b)
+print(a <= b)
+print(a > b)
+print(a >= b)
+
+print("")
+print("")
+
+# Operating with logical (Boolean) variables
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
+print("")
+print("")
+
+print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+print("")
+print("")
+
+print(not False)
+print(not True)
+
+print("")
+print("")
+
+# Cast to boolean
+print(bool(-1))
+print(bool(1))
+print(bool(0))
+print(bool(""))
+print(bool("text"))
+print(bool(None))
+
+# Check variable type
+variable1 = "text"
+print(type(variable1))
+print(type(variable1) is str)
+
+variable2 = 123
+print(type(variable2))
+print(type(variable2) is int)
+
---
+++ task.py
@@ -0,0 +1,11 @@
+a = 5
+b = 8
+
+perimeter = 2 * (a + b)
+print(perimeter)
+
+grade1 = 34
+grade2 = 86
+grade = 74
+
+average = mean()
